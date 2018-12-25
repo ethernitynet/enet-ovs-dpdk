3f0143f37b8086773730668376f693f71048cd7e
Yotam Ramon <yotam@ethernitynet.com>
2018-12-25 08:59:30 -0500

    organize env

--- env/enet_fwd_env.sh
+++ env/enet_fwd_env.sh
@@ -11,3 +11,7 @@ export ENET_ENG_FLAG_VLAN_PUSH="-hType ${ENET_ENG_ID_VLAN_PUSH}"
 
 export ENET_FWD_VLAN_POP_PATTERN='priority=%d,in_port=%s,dl_vlan=%d,actions=strip_vlan,output:%s'
 export ENET_FWD_VLAN_PUSH_PATTERN='priority=%d,in_port=%s,actions=push_vlan:0x8100,mod_vlan_vid:%d,output:%s'
+
+export OVS_FLOW_PORT_SUBNET_PAIR_SPEC='in_port=%d,ip,nw_src=%s,nw_dst=%s'
+export OVS_FLOW_PORT_SUBNET_PAIR_VLAN_SWAP='priority=%d,in_port=%d,dl_vlan=%d,ip,nw_src=%s,nw_dst=%s,actions=mod_vlan_vid:%d,goto_table=%d'
+export OVS_FLOW_PORT_SUBNET_PAIR_L3FWD_VLAN_SWAP='priority=%d,in_port=%d,dl_vlan=%d,ip,nw_src=%s,nw_dst=%s,actions=mod_vlan_vid:%d,mod_dl_src=%s,mod_dl_dst=%s,dec_ttl,output:in_port'
--- env/enet_ovs_env.sh
+++ env/enet_ovs_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 export ENET_NIC_BR=enetbr0
+export ENET_NIC_INTERFACE=ens1f2
+export ENET_NIC_PCI="0000:01:00.2"
