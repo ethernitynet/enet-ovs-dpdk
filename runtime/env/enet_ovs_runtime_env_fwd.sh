c91254bbbf001f86fa96040a03de59f9835e553a
root <erezb@ethernitynet.com>
2019-01-10 03:05:03 -0500

    rename

--- runtime/env/enet_ovs_runtime_env_fwd.sh
+++ runtime/env/enet_ovs_runtime_env_fwd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export WS="\s\{1,\}"
+export WS='\s\{1,\}'
+export NUM='[0-9][0-9]*'
+export MAC='[0-9A-Fa-f\:][0-9A-Fa-f\:]*'
 export ENET_FLAG_CLASSIFY_UNTAGGED="-l2Type 0"
 export ENET_FLAG_CLASSIFY_TAGGED="-l2Type 1"
 
@@ -15,3 +17,6 @@ export ENET_FWD_VLAN_PUSH_PATTERN='priority=%d,in_port=%s,actions=push_vlan:0x81
 export OVS_FLOW_PORT_SUBNET_PAIR_SPEC='in_port=%d,ip,nw_src=%s,nw_dst=%s'
 export OVS_FLOW_PORT_SUBNET_PAIR_VLAN_SWAP='priority=%d,in_port=%d,dl_vlan=%d,ip,nw_src=%s,nw_dst=%s,actions=mod_vlan_vid:%d,goto_table=%d'
 export OVS_FLOW_PORT_SUBNET_PAIR_L3FWD_VLAN_SWAP='priority=%d,in_port=%d,dl_vlan=%d,ip,nw_src=%s,nw_dst=%s,actions=mod_vlan_vid:%d,mod_dl_src=%s,mod_dl_dst=%s,dec_ttl,output:in_port'
+
+export OVS_FLOW_PORT_ARP_SUBNET_PAIR_DO_L2FWD='priority=%d,in_port=%d,dl_vlan=%d,dl_type=0x0806,nw_src=%s,nw_dst=%s,output:%d'
+export OVS_FLOW_PORT_SMAC_ARP_SUBNET_PAIR_DO_SET_DMAC_L2FWD='priority=%d,in_port=%d,dl_src=%s,dl_type=0x0806,nw_src=%s,nw_dst=%s,actions=push_vlan:0x8100,mod_vlan_vid=%d,output:%d'
